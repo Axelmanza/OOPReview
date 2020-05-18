a62c4e5991a4f7602a24898c1a9a26d0d917dc3c
Axelmanza <manzanares712@gmail.com>
2020-05-18 18:16:05 +0200

    segundo ejercicio

---
+++ moviles.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+var Mobile_1 = require("./Mobile");
+var Nokia = new Mobile_1.Mobile('Nokia', '3210', 'NK', 64, 'blue', false, 2, 180);
+var iPhone = new Mobile_1.Mobile('iPhone', '3G', 'AAPL', 128, 'black', true, 3, 590);
+var Samsung = new Mobile_1.Mobile('Samsung', 'Galaxy 10', 'Smsng', 64, 'withe', true, 1, 300);
+console.log(Nokia);
+console.log(iPhone);
+console.log(Samsung);
+Nokia.is5G = true;
+Nokia.cameraNumber = 4;
+console.log(Nokia, iPhone, Samsung);
